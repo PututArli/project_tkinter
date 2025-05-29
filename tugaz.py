4e1fbf8de89ca58b90c36d7565d3c18e9c9779a0
Athallahwildan <athallahwildanrafi23@gmail.com>
2025-05-29 10:36:07 +0700

    ubah menjadi class

--- tugaz.py
+++ tugaz.py
@@ -5,14 +5,16 @@ from PIL import Image, ImageTk
 customtkinter.set_appearance_mode("dark") 
 customtkinter.set_default_color_theme("blue")
 
-app = customtkinter.CTk()
-app.title("Login Page")
-app.geometry("800x500")
+class Login:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Login Page")
+        self.root.geometry("800x500")
 
-title = customtkinter.CTkLabel(app, text="Login Page", font=("Arial", 20, "bold"))
-title.pack(pady=20, padx=10)
+        self.label_judul = customtkinter.CTkLabel(root, text="Login Page", font=("Arial", 20, "bold"))
+        self.label_judul.pack(pady=20, padx=10)
 
-
-
-
-app.mainloop()
+if __name__ == "__main__":
+    root = customtkinter.CTk()
+    app = Login(root)
+    root.mainloop()
