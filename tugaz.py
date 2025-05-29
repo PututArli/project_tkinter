778348b9b7661e7797f8d7b175c11cb0e567439b
WisnuXiaomi <remensupriatin69@gmail.com>
2025-05-29 12:26:07 +0700

    Add Button

--- tugaz.py
+++ tugaz.py
@@ -11,14 +11,21 @@ class Login:
         self.root.title("Login Page")
         self.root.geometry("800x500")
 
-        self.label_judul = customtkinter.CTkLabel(root, text="Login Page", font=("Arial", 20, "bold"))
-        self.label_judul.pack(pady=20, padx=10)
 
         self.frame_kiri = customtkinter.CTkFrame(root, width=400, height=500)
         self.frame_kiri.pack(side="left", fill="both", expand=True)
 
         self.frame_kanan = customtkinter.CTkFrame(root, width=400, height=500, fg_color="purple")
         self.frame_kanan.pack(side="right", fill="both", expand=True)
+
+        self.label_judul = customtkinter.CTkLabel(self.frame_kiri, text="SIGN IN", font=("Arial", 20, "bold"))
+        self.label_judul.pack(pady=20, padx=10)
+
+        self.tengah_framekiri = customtkinter.CTkFrame(self.frame_kiri)
+        self.tengah_framekiri.pack(expand = True)
+
+        self.button = customtkinter.CTkButton(self.tengah_framekiri , text= "Login")
+        self.button.pack(anchor = "center")
 
 if __name__ == "__main__":
     root = customtkinter.CTk()
