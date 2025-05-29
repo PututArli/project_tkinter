4504899ba6f71da2601f4f30ef70b08f9b064cec
DzakiyArr <miqdaddzaky143@gmail.com>
2025-05-30 06:17:33 +0700

    hmmm

--- tugaz.py
+++ tugaz.py
@@ -32,14 +32,14 @@ class Login:
         self.label_judul = customtkinter.CTkLabel(self.tengah_framekiri, text="SIGN IN", font=("Times New Roman", 20, "bold"))
         self.label_judul.pack(pady=20, padx=10)
 
-        self.label_judul = customtkinter.CTkLabel(self.frame_kanan, text="Welcome", font=("Times New Roman", 50, "bold"))
-        self.label_judul.pack(pady=(150,15), padx=10)
-
         self.entry_username = customtkinter.CTkEntry(self.tengah_framekiri,placeholder_text="Username", width=200)
         self.entry_username.pack(pady=(0,15), padx=20)
 
         self.entry_password = customtkinter.CTkEntry(self.tengah_framekiri, width=200,placeholder_text="Password", show="*")
         self.entry_password.pack(pady=(15,0), padx=30)
+
+        self.label_welcome = customtkinter.CTkLabel(self.frame_kanan, text="Welcome", font=("Times New Roman", 50, "bold"))
+        self.label_welcome.place(relx=0.5, rely=0.3, anchor="center")
 
         self.button = customtkinter.CTkButton(self.tengah_framekiri , text= "Login")
         self.button.pack(anchor = "center", pady=(30,20), padx=40)
