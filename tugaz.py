2ecaea112e22cc7e05d0c7e39d452d1e81ff12ed
RafaelPututArli <rafaelpututarli@gmail.com>
2025-05-30 10:17:22 +0700

    Fix fullscreen (belum fix sih), add login method

--- tugaz.py
+++ tugaz.py
@@ -6,7 +6,7 @@ from PIL import Image, ImageTk
 customtkinter.set_appearance_mode("dark") 
 customtkinter.set_default_color_theme("blue")
 
-class Login:
+class Welcome:
     def __init__(self, root):
         self.root = root
         self.root.title("Login Page")
@@ -27,27 +27,38 @@ class Login:
         self.frame_kanan.pack(side="left", fill="both", expand=True)
 
         self.tengah_framekiri = customtkinter.CTkFrame(self.frame_kiri)
-        self.tengah_framekiri.pack(expand = True)
+        self.tengah_framekiri.place(relx=0.5, rely=0.5, anchor="center", relwidth=0.6, relheight=0.5)
         
         self.label_judul = customtkinter.CTkLabel(self.tengah_framekiri, text="SIGN IN", font=("Times New Roman", 20, "bold"))
         self.label_judul.pack(pady=20, padx=10)
 
         self.entry_username = customtkinter.CTkEntry(self.tengah_framekiri,placeholder_text="Username", width=200)
-        self.entry_username.pack(pady=(0,15), padx=20)
+        self.entry_username.pack(fill="both", pady=(0,15), padx=20)
 
         self.entry_password = customtkinter.CTkEntry(self.tengah_framekiri, width=200,placeholder_text="Password", show="*")
-        self.entry_password.pack(pady=(15,0), padx=30)
+        self.entry_password.pack(fill="both", pady=(15,0), padx=30)
 
         self.label_welcome = customtkinter.CTkLabel(self.frame_kanan, text="Welcome", font=("Times New Roman", 50, "bold"))
         self.label_welcome.place(relx=0.5, rely=0.25, anchor="center")
 
-        self.subtext1 = customtkinter.CTkLabel(self.frame_kanan, text="isi kata2 disini", font=("Arial", 16, "italic"))
+        self.subtext1 = customtkinter.CTkLabel(self.frame_kanan, text="lorem ipsum dolor", font=("Arial", 16, "italic"))
         self.subtext1.place(relx=0.5, rely=0.325, anchor="center")
 
-        self.button = customtkinter.CTkButton(self.tengah_framekiri , text= "Login")
-        self.button.pack(anchor = "center", pady=(30,20), padx=40)
+        self.button = customtkinter.CTkButton(self.tengah_framekiri , text= "Login", command=self.Login)
+        self.button.pack(fill="both", anchor = "center", pady=(30,20), padx=40)
+
+    def Login(self):
+        username = self.entry_username.get()
+        password = self.entry_password.get()
+        if username == "WisnuXiaomi" and password == "123":
+            messagebox.showinfo("Login Sukses!", "selamat datang")
+        else:
+            messagebox.showerror("Login Gagal", "coba lagi!")
+
+
+        
     
 if __name__ == "__main__":
     root = customtkinter.CTk()
-    app = Login(root)
+    app = Welcome(root)
     root.mainloop()
