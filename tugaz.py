33f96b185d637443e2e214d20e25b912109c9b93
WisnuXiaomi <remensupriatin69@gmail.com>
2025-06-11 23:11:54 +0700

    menyicil bagian skor

--- tugaz.py
+++ tugaz.py
@@ -19,7 +19,7 @@ class Welcome:
         self.frame_tengah.pack(fill="both", expand=True)
 
 
-        self.bg_image = customtkinter.CTkImage(Image.open("welcome.png"), size=(1200, 700))
+        self.bg_image = customtkinter.CTkImage(Image.open("D:/SDA TKINTER/project_tkinter/welcome.png"), size=(1200, 700))
         self.bg_label = customtkinter.CTkLabel(self.frame_tengah, image=self.bg_image, text="")
         self.bg_label.place(relx=0.5, rely=0.5, anchor="center")
 
@@ -72,7 +72,7 @@ class Welcome:
         self.frame_about = customtkinter.CTkFrame(self.root,fg_color="#fdf6e3")
 
 
-        self.bg_image_about = customtkinter.CTkImage(Image.open("anggota.png"), size=(1200, 700))
+        self.bg_image_about = customtkinter.CTkImage(Image.open("D:/SDA TKINTER/project_tkinter/anggota.png"), size=(1200, 700))
         self.bg_label_about = customtkinter.CTkLabel(self.frame_about, image=self.bg_image_about, text="")
         self.bg_label_about.place(relx=0.5, rely=0.5, anchor="center") 
 
@@ -128,112 +128,110 @@ class Welcome:
         
 
         # Frame SDA
-        self.frame_sda = customtkinter.CTkFrame(self.root, fg_color="#fdf6e3")
+        self.frame_sda = customtkinter.CTkFrame(self.root,fg_color="black",width=1000,height=1000)
+
+        self.frame_score_kiri = customtkinter.CTkFrame(self.frame_sda,fg_color="#005EFF",width=500,height=500)
+        self.frame_score_kanan = customtkinter.CTkFrame(self.frame_sda,fg_color="#FF0000", width=500,height=500)
+        self.frame_atas = customtkinter.CTkFrame(self.frame_sda,fg_color="#000000",width=500, height=100)
 
         # Score
-        self.label_about = customtkinter.CTkLabel(self.frame_sda,
-                                                    text="Score",
-                                                    font=("Times New Roman", 30, "bold"),
-                                                    text_color="#d19a03",
-                                                    fg_color="#fdf6e3")
-        self.label_about.place(relx=0.5, rely=0.045, anchor="center")
 
-        self.label_division = customtkinter.CTkLabel(self.frame_sda,
-                                                     text="Division:",
-                                                    font=("Times New Roman", 20, "bold"),
-                                                    text_color="#d19a03",
-                                                    fg_color="#fdf6e3")
+        self.label_division = customtkinter.CTkLabel(self.frame_atas,
+                                                    text="Division:",
+                                                    font=("Comic Sans", 20, "bold"),
+                                                    text_color="#ffffff",
+                                                    fg_color="#000000")
         self.label_division.place(relx=0.4, rely=0.075)
 
-        self.entry_division = customtkinter.CTkEntry(self.frame_sda, width=175,
-                                                     font=("Times New Roman", 20, "bold"),
-                                                     text_color="#d19a03",
-                                                     fg_color="#fdf6e3")
+        self.entry_division = customtkinter.CTkEntry(self.frame_atas, width=175,
+                                                     font=("Comic Sans", 20, "bold"),
+                                                     text_color="#FFFFFF",
+                                                     fg_color="#000000")
         self.entry_division.place(relx=0.465, rely=0.075)
-
         # nama kiri
-        self.label_name1 = customtkinter.CTkLabel(self.frame_sda,
-                                                     text="Name",
-                                                    font=("Times New Roman", 20, "bold"),
-                                                    text_color="#d19a03",
-                                                    fg_color="#fdf6e3")
-        self.label_name1.place(relx=0.125, rely=0.1)
-        self.entry_name1 = customtkinter.CTkEntry(self.frame_sda, width=150,
-                                                     font=("Times New Roman", 20, "bold"),
-                                                     text_color="#d19a03",
-                                                     fg_color="#fdf6e3")
-        self.entry_name1.place(relx=0.085, rely=0.135)
+        self.label_name1 = customtkinter.CTkLabel(self.frame_atas,
+                                                     text="Ao",
+                                                    font=("Comic Sans", 50, "bold"),
+                                                    text_color="#005EFF",
+                                                    fg_color="#000000")
+        self.label_name1.place(relx=0.025, rely=0.40)
+        self.entry_name1 = customtkinter.CTkEntry(self.frame_atas, width=150,
+                                                     font=("Comic Sans", 20, "bold"),
+                                                     text_color="#ffffff",
+                                                     fg_color="#000000")
+        self.entry_name1.place(relx=0.070, rely=0.60)
 
         # score kiri
-        self.label_score1 = customtkinter.CTkLabel(self.frame_sda,
+        self.label_score1 = customtkinter.CTkLabel(self.frame_score_kiri,
                                                      text="0",
-                                                    font=("Times New Roman", 200, "bold"),
-                                                    text_color="#d19a03",
-                                                    fg_color="#fdf6e3")
-        self.label_score1.place(relx=0.1, rely=0.195)
-        self.button_score11 = customtkinter.CTkButton(self.frame_sda, text="+1",
-                                                    font=("Times New Roman", 20, "bold"),
-                                                    fg_color="#d19a03",
-                                                    hover_color="#f0b400",
+                                                    font=("Comic Sans", 200, "bold"),
+                                                    text_color="#ffffff",
+                                                    fg_color="#005EFF")
+        self.label_score1.place(relx=0.15, rely=0.195)
+
+        self.button_score11 = customtkinter.CTkButton(self.frame_score_kiri, text="+1",
+                                                    font=("Comic Sans", 20, "bold"),
+                                                    fg_color="#25bc1f",
+                                                    hover_color="#25e61e",
                                                     text_color="white",
                                                     command=self.add_kiri)
-        self.button_score11.place(relx=0.115, rely=0.5, anchor="center",
+        self.button_score11.place(relx=0.165, rely=0.5, anchor="center",
                                 relwidth=0.05, relheight=0.04)
         
-        self.button_score12 = customtkinter.CTkButton(self.frame_sda, text="-1",
-                                                    font=("Times New Roman", 20, "bold"),
-                                                    fg_color="#d19a03",
-                                                    hover_color="#f0b400",
+        self.button_score12 = customtkinter.CTkButton(self.frame_score_kiri, text="-1",
+                                                    font=("Comic Sans", 20, "bold"),
+                                                    fg_color="#0c28b6",
+                                                    hover_color="#0d2fd8",
                                                     text_color="white",
                                                     command=self.sub_kiri)
-        self.button_score12.place(relx=0.185, rely=0.5, anchor="center",
+        self.button_score12.place(relx=0.265, rely=0.5, anchor="center",
                                 relwidth=0.05, relheight=0.04)
 
         # nama kanan
-        self.label_name2 = customtkinter.CTkLabel(self.frame_sda,
-                                                     text="Name",
-                                                    font=("Times New Roman", 20, "bold"),
-                                                    text_color="#d19a03",
-                                                    fg_color="#fdf6e3")
-        self.label_name2.place(relx=0.8, rely=0.1)
-        self.entry_name2 = customtkinter.CTkEntry(self.frame_sda, width=150,
-                                                     font=("Times New Roman", 20, "bold"),
-                                                     text_color="#d19a03",
-                                                     fg_color="#fdf6e3")
-        self.entry_name2.place(relx=0.76, rely=0.135)
+        self.label_name2 = customtkinter.CTkLabel(self.frame_atas,
+                                                     text="Aka",
+                                                    font=("Comic Sans", 50, "bold"),
+                                                    text_color="#ff0000",
+                                                    fg_color="#000000")
+        self.label_name2.place(relx=0.85, rely=0.40)
+        self.entry_name2 = customtkinter.CTkEntry(self.frame_atas, width=150,
+                                                     font=("Comic Sans", 20, "bold"),
+                                                     text_color="#ffffff",
+                                                     fg_color="#000000")
+        self.entry_name2.place(relx=0.745, rely=0.60)
         
         # score kanan
-        self.label_score2 = customtkinter.CTkLabel(self.frame_sda,
+        self.label_score2 = customtkinter.CTkLabel(self.frame_score_kanan,
                                                      text="0",
-                                                    font=("Times New Roman", 200, "bold"),
-                                                    text_color="#d19a03",
-                                                    fg_color="#fdf6e3")
-        self.label_score2.place(relx=0.785, rely=0.195)
-        self.button_score21 = customtkinter.CTkButton(self.frame_sda, text="+1",
-                                                    font=("Times New Roman", 20, "bold"),
-                                                    fg_color="#d19a03",
-                                                    hover_color="#f0b400",
+                                                    font=("Comic Sans", 200, "bold"),
+                                                    text_color="#ffffff",
+                                                    fg_color="#FF0000")
+        self.label_score2.place(relx=0.685, rely=0.195)
+        self.button_score21 = customtkinter.CTkButton(self.frame_score_kanan, text="+1",
+                                                    font=("Comic Sans", 20, "bold"),
+                                                    fg_color="#25bc1f",
+                                                    hover_color="#25e61e",
                                                     text_color="white",
                                                     command=self.add_kanan)
-        self.button_score21.place(relx=0.795, rely=0.5, anchor="center",
+        self.button_score21.place(relx=0.695, rely=0.5, anchor="center",
                                 relwidth=0.05, relheight=0.04)
         
-        self.button_score22 = customtkinter.CTkButton(self.frame_sda, text="-1",
-                                                    font=("Times New Roman", 20, "bold"),
-                                                    fg_color="#d19a03",
-                                                    hover_color="#f0b400",
+        self.button_score22 = customtkinter.CTkButton(self.frame_score_kanan, text="-1",
+                                                    font=("Comic Sans", 20, "bold"),
+                                                    fg_color="#0c28b6",
+                                                    hover_color="#0d2fd8",
                                                     text_color="white",
                                                     command=self.sub_kanan)
-        self.button_score22.place(relx=0.865, rely=0.5, anchor="center",
+        self.button_score22.place(relx=0.805, rely=0.5, anchor="center",
                                 relwidth=0.05, relheight=0.04)
-
 
         # Tombol back Score
         self.button_back2 = customtkinter.CTkButton(self.frame_sda, text="Back",
-                                                    font=("Times New Roman", 20, "bold"),
+                                                    font=("Comic Sans", 20, "bold"),
                                                     fg_color="#d19a03",
                                                     hover_color="#f0b400",
                                                     text_color="white",
+                                                    corner_radius= 30,
                                                     command=self.welcomepage)
         self.button_back2.place(relx=0.95, rely=0.05, anchor="center",
                                 relwidth=0.05, relheight=0.04)
@@ -250,6 +248,9 @@ class Welcome:
     def sda(self):
         self.frame_tengah.pack_forget()
         self.frame_sda.pack(fill="both", expand=True)
+        self.frame_atas.pack(side="top",fill="x", expand=False)
+        self.frame_score_kanan.pack(side= "right",fill="both", expand=True)
+        self.frame_score_kiri.pack(side= "left",fill= "both" , expand=True)
 
     def add_kiri(self):
         self.kiri_score += 1
